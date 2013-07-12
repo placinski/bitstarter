4a2f8367bfd7ca324d85d18faaefd939598c57b0
Ubuntu <ubuntu@ip-172-31-4-255.us-west-2.compute.internal>
2013-07-12 17:47:34 +0000

    Updated web and added index

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+var buffer = fs.readFile('index.html');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
